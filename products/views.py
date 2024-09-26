4a72553ef8cbd91dd0246e44b03aa63c6f3a07d1
renjumr87 <renjumr87@gmail.com>
2024-09-27 00:40:58 +0530

    Layout Design Started

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
+    
+# List Products
+def list_products(request):
+    """_summary_"""
+    return render(request, 'products.html')
+
+# Product Details
+def detail_products(request):
+    return render(request, 'prducts.html')
