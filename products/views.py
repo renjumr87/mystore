44b5638414ad6a8c8a849649ec86cb6bb17b0cfe
renjumr87 <renjumr87@gmail.com>
2024-10-31 04:54:05 +0530

    Product module completed

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from . models import Product
+from django.core.paginator import Paginator
 # Create your views here.
 def index(request):
     return render(request,'index.html')
@@ -7,7 +8,14 @@ def index(request):
 # List Products
 def list_products(request):
     """_summary_"""
-    return render(request, 'products.html')
+    page=1
+    if request.GET:
+        page=request.GET.get('page',1)
+    product_list=Product.objects.all()
+    product_paginator=Paginator(product_list,2)
+    product_list=product_paginator.get_page(page)
+    context={'products':product_list}
+    return render(request, 'products.html',context)
 
 # Product Details
 def detail_products(request):
